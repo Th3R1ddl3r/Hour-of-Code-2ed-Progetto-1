af5647b660aeab69dd53c1588c7e8713df701e42
Th3R1ddl3r <lorenzo.allegramente@gmail.com>
2021-12-07 20:06:24 +0100

    Solution

--- password_verifier.py
+++ password_verifier.py
@@ -1,24 +1,34 @@
 import re
 
+valida = True
+
 while True:
-    
+    password = input("Dimmi la tua password: ")
 
-    # La password deve essere lunga almeno 8 caratteri
-    
+    if len(password) <8:
+        print("La password deve essere lunga almeno 8 caratteri")
+        valida = False
 
-    # La password deve contenere almeno un carattere maiuscolo
-    
+    if not re.search("[A-Z]+", password):
+        print ("La password deve contenere almeno un carattere maiuscolo")
+        valida = False
 
-    # La password deve contenere almeno un carattere minuscolo
+    if not re.search("[a-z]+", password):
+        print ("La password deve contenere almeno un carattere minuscolo")
+        valida = False
     
+    if not re.search("[0-9]+", password):
+        print ("La password deve contenere almeno un numero")
+        valida = False
 
-    # La password deve contenere almeno un numero
+    if not re.search("[\.,_-]+", password):
+        print ("La password deve contenere almeno carattere speciale: .,_-")
+        valida = False
     
+    if valida:  
+        print("La password è valida")
+        break
+    else:
+        continue
 
-    # La password deve contenere almeno un carattere speciale: .,_-
-    
-    
-    pass
 
-
-print("Chiudo il programma.")
